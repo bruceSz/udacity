c375eb4f6b4464e1e9cdd0ff026986acf18ed488
brucesz <zsyuyizhang@gmail.com>
2019-06-23 17:22:43 +0800

    code done need pass test fro hs1

--- ai_for_robot/hs1.py
+++ ai_for_robot/hs1.py
@@ -42,20 +42,207 @@ world2D = [['green','red','green'],
 
 measurements2D = ['red']
 motions2D = [[0,0],[0,1]]
-
-def sense2D(p, Z):
-    q  = []
-    for i in range(len(p)):
-        for j in range(len(p[0])):
-            hit  = (Z == p[i][j])
-            q.append(p[i][j]*(pHit*(hit)+pMiss(1-hit)))
-    tmp_s = 0.0
-
-    return  q
+p_move = 0.5
 
 
-def move2D(p,U):
-    for i in range(len())
+def show(p):
+    rows = ['[' + ','.join(map(lambda x: '{0:.5f}'.format(x),r)) + ']' for r in p]
+    print '[' + ',\n '.join(rows) + ']'
+
+
+def localize(colors, measurements, motions, sensor_right, p_move):
+    pinit = 1.0 / float(len(colors)) / float(len(colors[0]))
+    p = [[pinit for row in range(len(colors[0]))] for col in range(len(colors))]
+    print "Before move."
+    show(p)
+
+
+    def sense2D(p, Z):
+        q  = []
+        for i in range(len(p)):
+            tmp = []
+            for j in range(len(p[0])):
+                hit = (Z == colors[i][j])
+                if hit:
+                    print "hit at", i,j
+                tmp.append(p[i][j]*(sensor_right*(hit)+(1-sensor_right)*(1-hit)))
+            q.append(tmp)
+        tmp_s = 0.0
+
+        for i in range(len(p)):
+            for j in range(len(p[0])):
+                tmp_s += q[i][j]
+
+        print "sense normal denominator.", tmp_s
+        for i in range(len(q)):
+            for j in range(len(q[0])):
+                q[i][j] = q[i][j]/tmp_s
+
+        return  q
+
+
+    def move2D(p,U):
+        if len(p) == 0:
+            return
+        col_b = 0
+        col_e = len(p[0])
+        row_b = 0
+        row_e = len(p)
+
+        q = []
+        # do init.
+        for i in range(len(p)):
+            tmp = []
+            for j in range(len(p[0])):
+                tmp.append(0.0)
+            q.append(tmp)
+
+        for i in range(len(p)):
+            for j in range(len(p[0])):
+                row, col = U[0], U[1]
+                from_r = (i - row)%row_e
+                from_c = (j - col)%col_e
+                if (from_r >=row_b and from_r < row_e) and (from_c >=col_b and from_c < col_e ):
+                    q[i][j] = p_move * p[from_r][from_c] #+ (1-p_move)*p[i][j]
+                    print "idx:", i, j , "from ", from_r, from_c, " with prob", q[i][j]
+                else:
+                    print "can not move from ", from_r, from_c,"inx of curr: ", i,j, "with row ", row_b, row_e, " col: ", col_b, col_e
+        tmp_s = 0.0
+
+        #for i in range(len(p)):
+        #    for j in range(len(p[0])):
+        #        tmp_s += q[i][j]
+
+        #print "sense normal denominator.", tmp_s
+        #for i in range(len(q)):
+        #    for j in range(len(q[0])):
+        #        q[i][j] = q[i][j] / tmp_s
+
+        return q
+
+    for i in range(len(measurements)):
+        p = sense2D(p,measurements[i])
+        print "after sense with measure ",i ,measurements[i]
+        show(p)
+        p = move2D(p,motions[i])
+        print "after move ", i, motions[i]
+        show(p)
+
+    print "after all."
+    show(p)
+
+
+def test1():
+    colors = [['G', 'G', 'G'],
+              ['G', 'R', 'G'],
+              ['G', 'G', 'G']]
+    measurements = ['R']
+    motions = [[0, 0]]
+    sensor_right = 1.0
+    p_move = 1.0
+    p = localize(colors, measurements, motions, sensor_right, p_move)
+    correct_answer = (
+        [[0.0, 0.0, 0.0],
+         [0.0, 1.0, 0.0],
+         [0.0, 0.0, 0.0]])
+
+
+def test2():
+    colors = [['G', 'G', 'G'],
+              ['G', 'R', 'R'],
+              ['G', 'G', 'G']]
+    measurements = ['R']
+    motions = [[0, 0]]
+    sensor_right = 1.0
+    p_move = 1.0
+    p = localize(colors, measurements, motions, sensor_right, p_move)
+    correct_answer = (
+        [[0.0, 0.0, 0.0],
+         [0.0, 0.5, 0.5],
+         [0.0, 0.0, 0.0]])
+
+def test3():
+    colors = [['G', 'G', 'G'],
+              ['G', 'R', 'R'],
+              ['G', 'G', 'G']]
+    measurements = ['R']
+    motions = [[0, 0]]
+    sensor_right = 0.8
+    p_move = 1.0
+    p = localize(colors, measurements, motions, sensor_right, p_move)
+    correct_answer = (
+        [[0.06666666666, 0.06666666666, 0.06666666666],
+         [0.06666666666, 0.26666666666, 0.26666666666],
+         [0.06666666666, 0.06666666666, 0.06666666666]])
+
+
+def test4():
+    colors = [['G', 'G', 'G'],
+              ['G', 'R', 'R'],
+              ['G', 'G', 'G']]
+    measurements = ['R', 'R']
+    motions = [[0, 0], [0, 1]]
+    sensor_right = 0.8
+    p_move = 1.0
+    p = localize(colors, measurements, motions, sensor_right, p_move)
+    correct_answer = (
+        [[0.03333333333, 0.03333333333, 0.03333333333],
+         [0.13333333333, 0.13333333333, 0.53333333333],
+         [0.03333333333, 0.03333333333, 0.03333333333]])
+
+def test5():
+    colors = [['G', 'G', 'G'],
+              ['G', 'R', 'R'],
+              ['G', 'G', 'G']]
+    measurements = ['R', 'R']
+    motions = [[0, 0], [0, 1]]
+    sensor_right = 1.0
+    p_move = 1.0
+    p = localize(colors, measurements, motions, sensor_right, p_move)
+    correct_answer = (
+        [[0.0, 0.0, 0.0],
+         [0.0, 0.0, 1.0],
+         [0.0, 0.0, 0.0]])
+
+
+def test6():
+    colors = [['G', 'G', 'G'],
+              ['G', 'R', 'R'],
+              ['G', 'G', 'G']]
+    measurements = ['R', 'R']
+    motions = [[0, 0], [0, 1]]
+    sensor_right = 0.8
+    p_move = 0.5
+    p = localize(colors, measurements, motions, sensor_right, p_move)
+    correct_answer = (
+        [[0.0289855072, 0.0289855072, 0.0289855072],
+         [0.0724637681, 0.2898550724, 0.4637681159],
+         [0.0289855072, 0.0289855072, 0.0289855072]])
+
+
+def test7():
+    colors = [['G', 'G', 'G'],
+              ['G', 'R', 'R'],
+              ['G', 'G', 'G']]
+    measurements = ['R', 'R']
+    motions = [[0, 0], [0, 1]]
+    sensor_right = 1.0
+    p_move = 0.5
+    p = localize(colors, measurements, motions, sensor_right, p_move)
+    correct_answer = (
+        [[0.0, 0.0, 0.0],
+         [0.0, 0.33333333, 0.66666666],
+         [0.0, 0.0, 0.0]])
+
+
+def main():
+    #test1()
+    #test2()
+    #test3()
+    #test4()
+    #test5()
+    #test6()
+    test7()
 
 
 
